59ac02f64e9edddc7616f91bea9344d147be6793
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-16 20:41:21 -0500

    Add: sum-of-two-integers - submission-0

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        print(bin(a), bin(b))
+        res = 0
+        carry = 0
+        for i in range(32): 
+            res |= (((carry) ^ (a >> i) ^ (b >> i)) & 1) << i
+            carry = ((a >> i) & (b >> i)) & 1 
+        #    print(((((carry) ^ (a >> i) ^ (b >> i)) & 1) << i), carry)
+
+        print(bin(10), bin(res))
+        return res if a >= 0 and b >= 0 else res ^ 0xffffffff
