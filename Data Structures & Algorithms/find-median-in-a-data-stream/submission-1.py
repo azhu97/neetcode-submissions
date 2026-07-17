d8f1c2d536ed4f68f01d253fa830b337e7d81d8e
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-17 15:56:04 -0500

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.py
@@ -0,0 +1,24 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.arr = [] 
+
+    def addNum(self, num: int) -> None:
+        self.arr.append(num)
+        # now we want to bubble down
+        i = len(self.arr) - 1
+        while i > 0 and self.arr[i - 1] > num:
+            self.arr[i], self.arr[i - 1] = self.arr[i - 1], self.arr[i]
+            i -= 1
+        print(f"Adding number {num}")
+        print(self.arr)
+
+    def findMedian(self) -> float:
+        if len(self.arr) % 2 == 1:
+            # if odd
+            return self.arr[len(self.arr) // 2]
+        else:
+            # if even 
+            return (self.arr[len(self.arr) // 2 - 1] + self.arr[len(self.arr) // 2]) / 2
+        
+        
