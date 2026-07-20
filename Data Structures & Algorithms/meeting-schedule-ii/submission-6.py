6a9547a79529658b510fac7e520b575b06084d06
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-20 11:13:42 -0500

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,26 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        heap = [] # contains (start, end) of rooms
+        # n + nlogn = n
+        res = 1
+        heapq.heappush(heap, (intervals[0].end))
+        # what scenario would we increase this
+        # we look at each room
+        # we add to the heap each time
+        # we want to see if
+        for interval in intervals[1::]:
+            # peak the top of the heap, and see if there is no conflict
+            if interval.start < heap[0]:
+                res += 1
+            heapq.heappush(heap, interval.end)
+        return res
