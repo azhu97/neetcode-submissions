8c2414dbf7257bb8d1047fbd1294c9fec3c0779f
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-22 14:59:07 -0500

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # how can we detect a cycle?
+        # if an edge connects two components that have already been "added" to the tree
+        # so we want to see if 
+        # 1. all nodes a reachable
+        # 2. there is no cycle
+        # the first thing, we have to build a mapping
+        mapping = defaultdict(list)
+        for u, v in edges:
+            mapping[u].append(v)
+            mapping[v].append(u)
+        print(mapping)
+        que = deque([edges[0][0]])
+        visted_set = set()
+        while que:
+            curr = que.popleft()
+            visted_set.add(curr)
+            for node in mapping[curr]:
+                if node in visted_set:
+                    continue
+                que.append(node)
+        
+        # we can automatically rule out its not a tree if not all nodes connect
+        if len(visted_set) != n:
+            return False
+        
+        # check for a cycle
+        visited_set = set()
+        for u, v in edges:
+            if u in visited_set and v in visited_set:
+                return False
+            visited_set.add(u)
+            visited_set.add(v)
+        return True
