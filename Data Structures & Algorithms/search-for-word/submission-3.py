7eaaf35e6543d060fe51763f05470c317062b116
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-22 10:42:03 -0500

    Add: search-for-word - submission-3

---
+++ Data Structures & Algorithms/search-for-word/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        res = False
+        def dfs(coord, i, visted_set):
+            nonlocal res
+            coords = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+            visted_set.add(coord)
+            if i >= len(word) - 1:
+                res = True
+                return
+            u, v = coord
+            for y, x in coords:
+                if u + y < 0 or v + x < 0 or u + y >= len(board) or v + x >= len(board[0]):
+                    continue
+                if board[u + y][v + x] == word[i + 1] and (u + y, v + x) not in visted_set:
+                    dfs((u + y, v + x), i + 1, visted_set)
+                    print((u + y, v + x) in visted_set)
+                    visted_set.remove((u + y, v + x))
+        
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if res == False and board[i][j] == word[0]:
+                    s = set()
+                    dfs((i, j), 0, s)
+
+        return res
