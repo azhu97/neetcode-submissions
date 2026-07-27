5d59caff51ce67915a513520238e202c537c779d
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-26 19:40:46 -0500

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # a string of upper 
+        # A -> 1, B -> 2, ..., Z -> 26
+        # there are mulitple ways to decode a message
+        # 1012 -> 10 1 2 | 10 12
+        # dp[i] tells us how many ways we can decode s[::i], including i
+        # dp[i] = dp[i-1] + + 1 + (1 if s[i - 1] <= 2 else 0)
+        dp = [0 for i in range(len(s))]
+        dp[0] = 1 if s[0] != '0' else 0
+        for i in range(1, len(s)):
+            dp[i] = dp[i - 1]
+            if s[i] != 0:
+                dp[i] += (1 if (0 < ord(s[i]) - ord('0') <= 6 and 0 < ord(s[i-1]) - ord('0') <= 2) else 0)
+            else:
+                dp[i] = dp[i - 2] if i - 2 >= 0 else 0
+        
+        print(dp)
+        return dp[-1]
