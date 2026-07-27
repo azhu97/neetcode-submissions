d4e45cfb2a87e5f27828a0adf091c3b7a8e58ec7
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-26 21:35:25 -0500

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # dp[i] = longest palindromic substring including s[i]
+        # at dp[i], we want to look for the largest number
+        # we can iterate from 0 to i - 1, call j, and see which dp[j] is the largest
+        # with the condition
+        # if dp[i] = dp[i - 1] + 1 if s[i] = s[i - dp[i - 1]] else 1
+        dp = [0 for i in range(len(s))]
+        dp[0] = 1
+        highest = -1
+        index = -1
+        for i in range(1, len(s)):
+            dp[i] = 1
+            for j in range(i):
+                # print(i, j)
+                # print(s[i] == s[j], (i - j - 1) <= dp[i - 1], dp[i - 1])
+                if s[i] == s[j] and (i - j - 1) <= dp[i - 1]:
+                    dp[i] = max(dp[i], i - j + 1)
+                    if dp[i] > highest:
+                        highest, index = dp[i], i
+        
+        print(dp)
+        return s[index - highest + 1 : index + 1]
