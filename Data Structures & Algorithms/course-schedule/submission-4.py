c75f782f9ccadde4914947e52a8f71414332cbf7
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-07-28 13:19:58 -0500

    Add: course-schedule - submission-4

---
+++ Data Structures & Algorithms/course-schedule/submission-4.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # return true only if 
+        # 1. all nodes are reachable 
+        # don't think even need to check
+        # 2. no cycle is formed from the edges -> cycle == livelock
+        adj_list = defaultdict(list)
+        class_require_prereq = set()
+        for u, v in prerequisites:
+            adj_list[v].append(u)
+            class_require_prereq.add(u)
+        s = set() # valid starting nodes
+        for i in range(numCourses):
+            if i not in class_require_prereq:
+                s.add(i)
+        
+        ### can't we just run bfs on each node? runtime would be same 
+        def bfs(start):
+            nonlocal adj_list
+            que = deque([start])
+            visited_set = set()
+            while que:
+                node = que.popleft()
+                visited_set.add(node)
+                for n in adj_list[node]:
+                    print(node, n)
+                    if n in visited_set:
+                        return False
+                    que.append(n)
+            return True
+        print("Here")
+        for i in s:
+            print("Here")
+            if not bfs(i):
+                return False
+        
+        if len(s) == 0:
+            return False
+        return True
+            
