425d6c1dbb41e232439035baffecbb0a65c1f206
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-08-10 22:07:30 -0500

    Add: regular-expression-matching - submission-2

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-2.py
@@ -0,0 +1,40 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # dp[i][j], does s[::i] match p[::j]:
+        # all starts false, except for 0, 0
+        # so when is dp[i][j] True? 
+        # we have to first look at s[i - 1], p[j - 1]
+        # 1. s and p match, dp[i][j] = dp[i - 1][j - 1]
+        # don't match 
+        # p not * or . -> do[i][j] = False, simply not possible
+        # p == ., dp[i][j] = dp[i - 1][i - j]
+        # p == *, dp[i][j] -> we have to check the previous 0 to i [j]
+        # find the nearest
+        # we don't want to greedily fill out p, take it slow 
+        # top to bottom, left to right
+        dp = [[False for j in range(len(p) + 1)] for i in range(len(s) + 1)]
+        dp[0][0] = True
+        p_set = set() # keep track of previous
+        for j in range(1, len(dp[0])):
+            for i in range(len(dp)):
+                print(i, j)
+                if i == 0:
+                    if p[j - 1] == '*':
+                        dp[i][j] = True
+                    continue
+                if s[i - 1] == p[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    # they don't match straight up
+                    if p[j - 1] == '.':
+                        dp[i][j] == dp[i - 1][j - 1]
+                    elif p[j - 1] == '*':
+                        if (dp[i][j-1] or dp[i - 1][j]) and s[i - 1] in p_set or '.' in p_set:
+                            dp[i][j] = True
+                        else:
+                            dp[i][j] = False
+            p_set.add(p[j - 1])
+
+        for arr in dp:
+            print(arr)
+        return dp[-1][-1]
