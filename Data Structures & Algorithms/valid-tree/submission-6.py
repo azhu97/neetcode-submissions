725644825e4503dea1cc5f82ba4e7de270f5518a
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-08-24 15:59:04 -0400

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            print("EARLY")
+            return False
+        
+        que = deque([0])
+        visited = set([0])
+        adj_list = defaultdict(list)
+        for u, v in edges:
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+        while que:
+            curr = que.popleft()
+            for ngbr in adj_list:
+                if ngbr in visited:
+                    continue
+                visited.add(ngbr)
+                que.append(ngbr)
+        return len(visited) == n
