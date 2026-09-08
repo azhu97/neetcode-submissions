9383624efd11a89767a4af8f03ce553c3c60695a
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-09-08 00:39:22 -0400

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(cost) > sum(gas):
+            return -1
+        # n gas stations on a CIRCULAR route
+        # two int arrs 
+        # gas -> amount of gas 
+        # cost -> amount of gas needed to travel from ith to ith+1 station
+        # gas storage unlimted, begin empty tank at ONE of the stations
+        # return the one index that you can travel across the entire track 
+        # brute force, try to naviagte the entire arr for each index
+
+        # at each gas station, we should try to take the most amount of gas possible
+        # what are the cases at each gas station? 
+        # 1. we start at this gas station
+        # 2. we continue from the previous gas station
+        res = -1
+        dif = -float('inf')
+        for i in range(len(gas)):
+            if (gas[i] - cost[i]) > dif:
+                res, dif = i, gas[i] - cost[i]
+        return res
