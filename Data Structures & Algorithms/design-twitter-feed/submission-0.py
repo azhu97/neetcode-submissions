9bbd604e9188880097bca5469580ee5694059768
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-09-09 18:28:40 -0400

    Add: design-twitter-feed - submission-0

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-0.py
@@ -0,0 +1,25 @@
+class Twitter:
+
+    def __init__(self):
+        self.clock = 0
+        self.follow_list = defaultdict(set) # account -> list of account id
+        self.account_tweets = defaultdict(set) # account -> list of tweet ids
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.account_tweets[userId].add((self.clock, tweetId))
+        self.clock += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        temp = []
+        for f_id in self.follow_list[userId]:
+            for tweet in self.account_tweets[f_id]:
+                temp.append(tweet)
+        temp.extend(self.account_tweets[userId])
+        temp.sort(reverse=True)
+        return [tweet for _, tweet in temp[:10]]
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follow_list[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follow_list[followerId].remove(followeeId)
