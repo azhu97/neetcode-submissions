5e37b489800dc0d5e02317e9b53c63cdb9f8ac8e
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-09-10 13:31:38 -0400

    Add: redundant-connection - submission-2

---
+++ Data Structures & Algorithms/redundant-connection/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        # implement using union find for practice
+        n = len(edges)
+        parent = {i:i for i in range(1, n + 1)}
+        rank = {i:0 for i in range(1, n + 1)}
+
+        def find(node):
+            if parent[node] != node:
+                return find(parent[node])
+            return node
+        
+        def merge(x, y):
+            nonlocal parent, rank 
+            root_x, root_y = find(x), find(y)
+            if root_x == root_y:
+                return False # can't merge, they're the same
+            
+            # check the rank before merging
+            if rank[root_x] > rank[root_y]:
+                parent[root_y] = root_x
+            elif rank[root_x] < rank[root_y]:
+                parent[root_x] = root_y
+            else:
+                # equal rank
+                parent[root_x] = root_y
+                rank[root_y] += 1
+        
+        def connected(x, y):
+            return find(x) == find(y)
+
+        for edge in edges:
+            x, y = edge
+            if connected(x, y):
+                return edge
+            # otherwise we merge them together
+            merge(x, y)
+        return [-1, -1] # should never happen 
