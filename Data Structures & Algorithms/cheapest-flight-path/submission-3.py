45161b9f43fb64ff4883a3e6ec0806e153b2bdfb
Alan Zhu <160082974+azhu97@users.noreply.github.com>
2026-09-11 11:58:42 -0400

    Add: cheapest-flight-path - submission-3

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-3.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # 1 directional array
+        # there are no duplicates flights, no loops either
+        adj_list = defaultdict(list)
+        for a, b, cost in flights:
+            adj_list[a].append((b, cost))
+        
+        def di(node):
+            # when adding the heap
+            # encapsulate
+            # 1. cost
+            # 2. remaining stops
+            # 3. node
+            heap = [(0, k, src)]
+            visited = set() # we should keep track of (cost, remaing, airport)
+            while heap and len(visited) < n:
+                cost, remain, airport = heapq.heappop(heap)
+                # print(cost, remain, airport)
+                if airport == dst:
+                    return cost
+                # visited.add((cost, remain, airport))
+                if remain < 0:
+                    # we can't jump anywhere from here
+                    # we can't even visit this stop
+                    continue
+                remain -= 1
+                for a, c in adj_list[airport]:
+                    # if (cost + c, remain, a) in visited:
+                        # continue
+                    heapq.heappush(heap, (cost + c, remain, a))
+            return -1 
+
+        return di(src)
+                
